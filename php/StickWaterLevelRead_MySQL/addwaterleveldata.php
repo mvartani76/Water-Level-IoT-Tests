08e81fee590e969fbb34278c3cb1b056616363b9
Mike Vartanian <mike.vartanian@gmail.com>
2015-10-28 09:19:05 -0400

    Corrected data that was inserted into table... No longer using temp1 and photo1...

--- php/StickWaterLevelRead_MySQL/addwaterleveldata.php
+++ php/StickWaterLevelRead_MySQL/addwaterleveldata.php
@@ -1,7 +1,7 @@
 <?php
    	include("db_connection.php");
    	$link=Connection();
-	$Sql="INSERT INTO stickwaterleveldata (temp1,photo1,DeviceType) VALUES ('".$_GET["waterlevel"]."', '".$_GET["water_present"]."', '".$_GET["DeviceType"]."')";     
+	$Sql="INSERT INTO stickwaterleveldata (waterlevel,water_present,DeviceType) VALUES ('".$_GET["waterlevel"]."', '".$_GET["water_present"]."', '".$_GET["DeviceType"]."')";     
 
 	if (!mysqli_query($link, $Sql)) {
     	echo "Error: " . $Sql . "<br>" . mysqli_error($link);
